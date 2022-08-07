74f63f3caae4dee4a3d6edb19fec494c9889c22d
Choi Eun Jeong <qhdrn67@naver.com>
2022-08-07 16:09:23 +0900

    Error test

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -28,6 +28,12 @@ describe('Calculator Test', () => {
     expect(cal.value).toBe(3);
   });
 
+  it('add should throw an error if value is greater than 100', () => {
+    expect(() => {
+      cal.add(101);
+    }).toThrow('Value can not be greater than 100');
+  });
+
   it('subtracts', () => {
     cal.set(1);
     cal.subtract(2);
