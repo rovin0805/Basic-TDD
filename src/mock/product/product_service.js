4d3502962da3ce18545ef3fe41cde8a993030534
Choi Eun Jeong <qhdrn67@naver.com>
2022-08-07 22:34:48 +0900

    stub test : good ver

---
+++ src/mock/product/product_service.js
@@ -0,0 +1,13 @@
+class ProductService {
+  constructor(ProductClient) {
+    this.productClient = ProductClient;
+  }
+
+  fetchAvailableItems() {
+    return this.productClient
+      .fetchItems()
+      .then((items) => items.filter((item) => item.available));
+  }
+}
+
+module.exports = ProductService;
